3e90e303b4ecaf5c9d0b0943253cbf1c1e64e507
MarjonDoping <marjondoping@gmail.com>
2024-05-27 17:12:50 +0800

    text

--- marjportfolio/marj-portfolio/src/Page/HeroPage.jsx
+++ marjportfolio/marj-portfolio/src/Page/HeroPage.jsx
@@ -21,7 +21,7 @@ const HeroPage = () => {
         />
 
         <div className="text-center">
-          <h1>
+          <h1 className="text-5xl font-bold mb-4">
             Need a <br />
             <span
               style={{
