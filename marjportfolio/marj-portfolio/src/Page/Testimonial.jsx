032a0778e83af2ec1542beb889ea2c3e6754d597
MarjonDoping <marjondoping@gmail.com>
2024-06-04 15:12:20 +0800

    changed text testimonials

--- marjportfolio/marj-portfolio/src/Page/Testimonial.jsx
+++ marjportfolio/marj-portfolio/src/Page/Testimonial.jsx
@@ -6,42 +6,42 @@ const testimonials = [
     id: 1,
     name: 'Angeline Seveses',
     position: 'Web Designer, NMS',
-    comment: '"Working with Marjon as a web designer was an absolute pleasure. Their ability to translate complex ideas into captivating visual representations is truly remarkable. They bring a unique flair to every project they undertake."',
+    comment: '"Working with Marjon as a web designer was an absolute pleasure. Her ability to translate complex ideas into captivating visual representations is truly remarkable. She bring a unique flair to every project they undertake."',
     avatarUrl: 'https://media.licdn.com/dms/image/D5603AQHccF0YAtOz1A/profile-displayphoto-shrink_800_800/0/1706614856823?e=1721865600&v=beta&t=YFQn2Xr4KAZUcKi6m1iw92uj9k5TdTJdZv9bQ-MYb1s',
   },
   {
     id: 2,
     name: 'Sheena Costales',
     position: 'Baguio, Wordpress Developer',
-    comment: '"As a graphic designer, Marjon has a rare talent for turning concepts into visual masterpieces. Their ability to capture the essence of our brand and convey it through stunning graphics has greatly enhanced our marketing efforts."',
+    comment: '"As a graphic designer, Marjon has a rare talent for turning concepts into visual masterpieces. Her ability to capture the essence of our brand and convey it through stunning graphics has greatly enhanced our marketing efforts."',
     avatarUrl: 'https://media.licdn.com/dms/image/D5603AQHHYp6snh2fYQ/profile-displayphoto-shrink_200_200/0/1648297828612?e=2147483647&v=beta&t=km53K-YMdbu7Rgune15kF16P7rqG8b4XAJQNKcKiqgw', // Add avatar URL here
   },
   {
     id: 3,
     name: 'Adelynne Malubay',
     position: 'Web Designer, NMS',
-    comment: '"As a web designer, Marj has an unmatched eye for detail and creativity. Their designs are not only visually stunning but also highly functional, creating seamless user experiences."',
+    comment: '"As a web designer, Marj has an unmatched eye for detail and creativity. Her designs are not only visually stunning but also highly functional, creating seamless user experiences."',
     avatarUrl: 'https://media.licdn.com/dms/image/D5603AQFx5ohGGpTikg/profile-displayphoto-shrink_200_200/0/1695915049947?e=2147483647&v=beta&t=j6ZAo_1XdkVHJWy5x21OTy4ybqMxT7w2jUhKH6TjVR4', // Add avatar URL here
   },
   {
     id: 4,
     name: 'Gilliane MKTG',
     position: 'SEO Marketing Manager',
-    comment: '"Thanks to your SEO skills, our websites visibility and organic traffic have significantly increased. Their strategic approach to optimization and attention to detail have made a noticeable impact on our online presence."',
+    comment: '"Thanks to your SEO skills, our websites visibility and organic traffic have significantly increased. Her strategic approach to optimization and attention to detail have made a noticeable impact on our online presence."',
     avatarUrl: 'https://img.freepik.com/free-photo/confident-middle-aged-woman-looking-camera_1262-17333.jpg', // Add avatar URL here
   },
   {
     id: 5,
     name: 'Celine Terrado',
     position: 'Designer, AI ',
-    comment: 'Ive had the pleasure of collaborating with Marjon on several projects, and each time, theyve proven themselves to be a highly skilled and reliable SEO expert. Their in-depth knowledge of search engine algorithms and dedication to staying updated on industry trends make them a valuable asset to any team."',
+    comment: 'Ive had the pleasure of collaborating with Marjon on several projects, and each time, she proven herself to be a highly skilled and reliable SEO expert. Her in-depth knowledge of search engine algorithms and dedication to staying updated on industry trends make them a valuable asset to any team."',
     avatarUrl: 'https://media.licdn.com/dms/image/D5603AQE5Abet53pGPA/profile-displayphoto-shrink_200_200/0/1711127161073?e=2147483647&v=beta&t=iyXDesA0NR1kb2drvGSq5447UP-KLChK_Ed-KT6TAfg', // Add avatar URL here
   },
   {
     id: 6,
     name: 'Ryan J.',
     position: 'Head Programmer, Cletos',
-    comment: 'As a web designer, Marjon has an innate ability to balance aesthetics with functionality. They have a keen eye for design and a deep understanding of user experience, resulting in websites that not only look great but also perform exceptionally well."',
+    comment: 'As a web designer, Marjon has an innate ability to balance aesthetics with functionality. She have a keen eye for design and a deep understanding of user experience, resulting in websites that not only look great but also perform exceptionally well."',
     avatarUrl: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODJ8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D', // Add avatar URL here
   },
   // Add more testimonials here
