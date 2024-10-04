1062a429701bfc0ea7833b0dc948a69a6120bda1
MarjonDoping <marjondoping@gmail.com>
2024-10-04 11:13:36 +0800

    mobile view

--- marjportfolio/marj-portfolio/tailwind.config.js
+++ marjportfolio/marj-portfolio/tailwind.config.js
@@ -7,6 +7,14 @@ export default {
     "./node_modules/flowbite/**/*.js",
   ],
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    
     extend: {
 
       backgroundImage: (theme) => ({
