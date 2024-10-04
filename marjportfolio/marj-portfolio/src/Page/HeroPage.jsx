e449f88539b3b768747dfc51ff58858611b89e34
MarjonDoping <marjondoping@gmail.com>
2024-10-04 11:12:41 +0800

    mobile view

--- marjportfolio/marj-portfolio/src/Page/HeroPage.jsx
+++ marjportfolio/marj-portfolio/src/Page/HeroPage.jsx
@@ -56,7 +56,6 @@ const HeroPage = () => {
             src="/images/WordPress logo.webp"
             alt="Image 2"
             className="w-16 h-16"
-            
           />
         </div>
         <div className="flex items-center justify-center mt-10 mx-4">
@@ -80,53 +79,54 @@ const HeroPage = () => {
                   />
                 </svg>
               </span>
-            </NoOutlinedButton> 
+            </NoOutlinedButton>
+            
           </Link>
 
           <Link to="/Wordpress">
-          <NoOutlinedButton>
-            <span className="flex items-center">
-              Wordpress Developer
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="25"
-                height="25"
-                viewBox="0 0 25 25"
-                fill="none"
-                className="ml-2"
-              >
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M9.5 7.25C9.08579 7.25 8.75 6.91421 8.75 6.5C8.75 6.08579 9.08579 5.75 9.5 5.75H18.5C18.9142 5.75 19.25 6.08579 19.25 6.5V15.5C19.25 15.9142 18.9142 16.25 18.5 16.25C18.0858 16.25 17.75 15.9142 17.75 15.5V8.31066L7.03033 19.0303C6.73744 19.3232 6.26256 19.3232 5.96967 19.0303C5.67678 18.7374 5.67678 18.2626 5.96967 17.9697L16.6893 7.25H9.5Z"
-                  fill="#F7B329"
-                />
-              </svg>
-            </span>
-          </NoOutlinedButton>
+            <NoOutlinedButton>
+              <span className="flex items-center">
+                Wordpress Developer
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="25"
+                  height="25"
+                  viewBox="0 0 25 25"
+                  fill="none"
+                  className="ml-2"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M9.5 7.25C9.08579 7.25 8.75 6.91421 8.75 6.5C8.75 6.08579 9.08579 5.75 9.5 5.75H18.5C18.9142 5.75 19.25 6.08579 19.25 6.5V15.5C19.25 15.9142 18.9142 16.25 18.5 16.25C18.0858 16.25 17.75 15.9142 17.75 15.5V8.31066L7.03033 19.0303C6.73744 19.3232 6.26256 19.3232 5.96967 19.0303C5.67678 18.7374 5.67678 18.2626 5.96967 17.9697L16.6893 7.25H9.5Z"
+                    fill="#F7B329"
+                  />
+                </svg>
+              </span>
+            </NoOutlinedButton>
           </Link>
 
           <Link to="/graphicDesigns">
-          <NoOutlinedButton>
-            <span className="flex items-center">
-              Graphic Designer
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="25"
-                height="25"
-                viewBox="0 0 25 25"
-                fill="none"
-                className="ml-2"
-              >
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M9.5 7.25C9.08579 7.25 8.75 6.91421 8.75 6.5C8.75 6.08579 9.08579 5.75 9.5 5.75H18.5C18.9142 5.75 19.25 6.08579 19.25 6.5V15.5C19.25 15.9142 18.9142 16.25 18.5 16.25C18.0858 16.25 17.75 15.9142 17.75 15.5V8.31066L7.03033 19.0303C6.73744 19.3232 6.26256 19.3232 5.96967 19.0303C5.67678 18.7374 5.67678 18.2626 5.96967 17.9697L16.6893 7.25H9.5Z"
-                  fill="#F7B329"
-                />
-              </svg>
-            </span>
-          </NoOutlinedButton>
+            <NoOutlinedButton>
+              <span className="flex items-center">
+                Graphic Designer
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="25"
+                  height="25"
+                  viewBox="0 0 25 25"
+                  fill="none"
+                  className="ml-2"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M9.5 7.25C9.08579 7.25 8.75 6.91421 8.75 6.5C8.75 6.08579 9.08579 5.75 9.5 5.75H18.5C18.9142 5.75 19.25 6.08579 19.25 6.5V15.5C19.25 15.9142 18.9142 16.25 18.5 16.25C18.0858 16.25 17.75 15.9142 17.75 15.5V8.31066L7.03033 19.0303C6.73744 19.3232 6.26256 19.3232 5.96967 19.0303C5.67678 18.7374 5.67678 18.2626 5.96967 17.9697L16.6893 7.25H9.5Z"
+                    fill="#F7B329"
+                  />
+                </svg>
+              </span>
+            </NoOutlinedButton>
           </Link>
         </div>
       </div>
